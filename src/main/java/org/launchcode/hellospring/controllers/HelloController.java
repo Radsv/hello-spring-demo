11b2879549538cacd1900c886fee40194fd59bf0
unknown <rads.veerapaneni@gmail.com>
2023-07-22 10:37:08 -0500

    1

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,7 +1,11 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
@@ -15,21 +19,26 @@ public class HelloController {
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
+
         return "Goodbye, Spring!";
     }
 
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    //@ResponseBody
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    //@ResponseBody
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        //String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
     }
 
 
@@ -37,5 +46,16 @@ public class HelloController {
     //@ResponseBody
     public String helloForm() {
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
+
     }
 }
